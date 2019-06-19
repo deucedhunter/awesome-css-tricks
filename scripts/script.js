1f8796a72f2e97d39ee66c30381adc92c77ff092
deuced <deucedhunter@gmail.com>
2019-06-19 17:33:40 +0200

    add dropdown list

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
     $("#burger-container").on('click', function(){
         $(this).toggleClass("open");
+
+        if ($('#nav-menu').hasClass('hide')) {
+            $('#nav-menu').removeClass("hide");
+        } else {
+            $('#nav-menu').addClass("hide");
+        }
+
     })
 })
